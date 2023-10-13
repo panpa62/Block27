ba5b74453a172b418c6cb06f5c9a91230963f89e
Pauline Pan <pan_pauline@yahoo.com>
2023-10-13 15:44:46 -0400

    Update before 2nd tab

--- React Forms/src/App.jsx
+++ React Forms/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Authenticate  from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
 
 import './App.css'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Authenticate />
+      <SignUpForm />
     </>
   )
 }
